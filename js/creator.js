3b63eb3e5e110dd4c5f99ad0eb198913ff629192
Fernando <palhares.f@gmail.com>
2025-10-23 18:16:59 -0300

    - QoL improvements in creator.js including auto-selecting generated link, removing unnecessary alert and updating button text when copying is successful

--- js/creator.js
+++ js/creator.js
@@ -29,12 +29,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     shareableLinkInput.value = shareableLink;
     generatedLinkPanel.style.display = "block";
+    shareableLinkInput.select();
   });
 
   copyLinkBtn.addEventListener("click", () => {
     shareableLinkInput.select();
     shareableLinkInput.setSelectionRange(0, 99999); // For mobile devices
-    document.execCommand("copy");
-    alert("Link copied to clipboard!");
+    navigator.clipboard.writeText(shareableLinkInput.value).then(() => {
+      copyLinkBtn.textContent = "Link Copied!";
+    })
   });
 });
